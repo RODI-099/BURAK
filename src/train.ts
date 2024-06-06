41ab3706120691f6f8e9de7661c4a6cd022fd126
RODI-099 <rodiiy099@gmail.com>
2024-06-06 16:03:50 +0500

    fix: modified train js

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,19 @@
-console.log('Hello World!')
+// H-TASK: 
+
+// shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+// MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+
+function filterPositiveIntegers(arr: number[]): string {
+    
+    const positiveValues: number[] = arr.filter(value => value > 0);
+  
+    
+    return positiveValues.join(',');
+  }
+  
+  
+  const numbers: number[] = [-1, 2, -3, 4, 0, 5];
+  const result: string = filterPositiveIntegers(numbers);
+  console.log(result); // Output: "2,4,5"
+  
