1503bdb70c23651e9cdb1c823b6cb145aeb30705
RODI-099 <rodiiy099@gmail.com>
2024-06-09 00:36:16 +0500

    fix: modified train js

--- src/train.ts
+++ src/train.ts
@@ -1,19 +1,38 @@
+// H2-TASK: 
+
+// Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141"
+
+
+
+function extractDigits(input: string): string {
+    const digits = input.match(/\d+/g);
+  
+    return digits ? digits.join('') : '';
+  }
+  
+  
+  const exampleString = "Hello MIT12, this is MIT14!";
+  const result = extractDigits(exampleString);
+  console.log(result); // 
+  
+
 // H-TASK: 
 
 // shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
 // MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 
 
-function getPositive(arr: number[]): string {
+// function getPositive(arr: number[]): string {
     
-    const positiveValues: number[] = arr.filter(value => value > 0);
+//     const positiveValues: number[] = arr.filter(value => value > 0);
   
     
-    return positiveValues.map(value => value.toString()).join("");
-  }
+//     return positiveValues.map(value => value.toString()).join("");
+//   }
   
   
-  const numbers: number[] = [-1, 2, -3, 4, 0, 5];
-  const result: string = getPositive(numbers);
-  console.log(result); // Output: "245"
+//   const numbers: number[] = [-1, 2, -3, 4, 0, 5];
+//   const result: string = getPositive(numbers);
+//   console.log(result); // Output: "245"
   
