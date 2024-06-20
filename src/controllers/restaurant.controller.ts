85eaafbd78f7e355d92c9205902e334316e629ec
RODI-099 <rodiiy099@gmail.com>
2024-06-20 17:39:24 +0500

    fix: modified controller

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -54,12 +54,14 @@ restaurantController.processSignup = async (req: AdminRequest, res: Response) =>
         req.session.save(function() {
             res.send(result);
         });
+       
 
     } catch (err) {
         console.log("Error, processSignup", err)
         const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG
         res.send(`<script> alert ("${message}"); window.location.replace('admin/signup')</script>`);
-        res.send(err);
+        
+        
     }
 
 };
