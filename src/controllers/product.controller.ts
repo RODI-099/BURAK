2be237364dbf9adaa988118f5eed0f9e408f6e53
RODI-099 <rodiiy099@gmail.com>
2024-06-23 20:13:46 +0500

    feat: upload files to server via multer

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -22,7 +22,8 @@ productController.getAllProducts = async (req: Request, res: Response) => {
 
 productController.createNewProduct = async (req: Request, res: Response) => {
     try {
-        console.log("createNewProduct")
+        console.log("createNewProduct");
+        res.send("DONE");
     } catch (err) {
         console.log("Error, createNewProduct", err);
         if(err instanceof Errors) res.status(err.code).json(err);
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,6 +2,7 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 import productController from "./controllers/product.controller";
+import makeUploader  from "./libs/utils/uploader";
 
 
 // Restaurant
@@ -11,7 +12,7 @@ routerAdmin
 .post("/login", restaurantController.processLogin);
 routerAdmin
 .get("/signup", restaurantController.getSignup)
-.post("/signup", restaurantController.processSignup);
+.post("/signup", makeUploader("members").single("memberImage"), restaurantController.processSignup);
 routerAdmin
 .get("/logout", restaurantController.logout)
 
@@ -26,7 +27,8 @@ routerAdmin.get(
 );
 routerAdmin.post(
     "/product/create",
-    restaurantController.verifyRestaurant, 
+    restaurantController.verifyRestaurant,
+    makeUploader("products").array("productImages", 5),
     productController.createNewProduct);
 routerAdmin.post(
     "/product/:id",
