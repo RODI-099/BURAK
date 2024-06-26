0c82e6d80b91c5c2f8184574927f4ba50e922a94
RODI-099 <rodiiy099@gmail.com>
2024-06-27 01:30:45 +0500

    feat: develop updateChosenUser api

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -36,5 +36,7 @@ routerAdmin.post(
      productController.updateChosenProduct)
 
 // User
-routerAdmin.get("/user/all", restaurantController.verifyRestaurant, restaurantController.getUsers)
+routerAdmin.get("/user/all", restaurantController.verifyRestaurant, restaurantController.getUsers);
+routerAdmin.post("/user/edit", restaurantController.verifyRestaurant, restaurantController.updateChosenUser);
+
 export default routerAdmin;
