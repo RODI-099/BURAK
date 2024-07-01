d3c5a05cdc3647999a82f9e6a7348fdde78b1353
RODI-099 <rodiiy099@gmail.com>
2024-07-02 01:10:44 +0500

    fix: admin project refactoring

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -3,10 +3,10 @@ console.log("Users frontend javascript file");
 $(function () {
     $(".member-status").on("change", function(e) {
         const id = e.target.id;
-        console.log("id:", id);
+        
 
         const memberStatus = $(`#${id}.member-status`).val();
-        console.log("memberStatus:", memberStatus)
+        
 
         // TODO Axios updateChosenUser
         axios.post("/admin/user/edit", {_id: id,
@@ -14,7 +14,7 @@ $(function () {
 }).then((response) => {
     console.log("response:", response);
     const result = response.data;
-    console.log("result:", result);
+    
 
     if(result.data) {
         console.log("User update!");
