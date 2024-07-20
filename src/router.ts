ae537941b0414dee6c625e73c1806d7935e69932
RODI-099 <rodiiy099@gmail.com>
2024-07-21 00:56:46 +0500

    feat modify getMemberDetail logic

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,19 @@ import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
 
-router
+/* MEMBER */
+router.post("/member/login", memberController.login);
+router.post("/member/signup", memberController.signup);
+router.post(
+  "/member/logout",
+  memberController.verifyAuth,
+  memberController.logout
+);
+router.get(
+  "/member/detail",
+  memberController.verifyAuth,
+  memberController.getMemberDetail
+);
 
-.post("/login", memberController.login);
-router
-
-.post("/signup", memberController.signup);
-
+/* MEMBER */
 export default router;
