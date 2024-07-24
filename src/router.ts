b3cb3fdbaf59c7ef09bed8c10fe2c02a7f94867d
RODI-099 <rodiiy099@gmail.com>
2024-07-24 20:41:14 +0500

    feat: develop getProducts rest api

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader"
+import productController from "./controllers/product.controller";
 /* MEMBER */
 router.get("/member/restaurant", memberController.getRestaurant );
 router.post("/member/login", memberController.login);
@@ -22,4 +23,7 @@ memberController.updateMember );
 
 router.get("/member/top-users", memberController.getTopUsers)
 
+// Products
+
+router.get("/product/all", productController.getProducts)
 export default router;
